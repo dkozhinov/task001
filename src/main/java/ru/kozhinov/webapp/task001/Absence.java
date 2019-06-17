4c4d818b862a0e693a4f3f3341f1c50f23376300
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-06-17 19:35:02 +1100

    Создана форма для ввода

---
+++ src/main/java/ru/kozhinov/webapp/task001/Absence.java
@@ -0,0 +1,11 @@
+package ru.kozhinov.webapp.task001;
+
+/**
+ * Java. Level 2. Lesson X. Homework.
+ *
+ * @author Dmitry Kozhinov d.kozhinov@mail.ru
+ * Created on 17.06.2019
+ */
+
+public class Absence {
+}
