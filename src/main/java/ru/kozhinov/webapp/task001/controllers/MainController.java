679f5b13bf0069f60823bf84b11261138bf221a5
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-06-19 01:27:16 +1100

    add dependency thymeleaf-spring5.

--- src/main/java/ru/kozhinov/webapp/task001/controllers/MainController.java
+++ src/main/java/ru/kozhinov/webapp/task001/controllers/MainController.java
@@ -11,8 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import ru.kozhinov.webapp.task001.service.AbsenceService;
+import ru.kozhinov.webapp.task001.repository.AbsenceRepository;
 import ru.kozhinov.webapp.task001.domain.Absence;
 
 import java.util.List;
@@ -23,36 +22,21 @@ public class MainController {
 
 
     @Autowired
-    private AbsenceService service;
+    private AbsenceRepository repository;
 
 
 
-    @SuppressWarnings("unchecked")
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public Model list(Model model) {
-
-        List<Absence> absences = service.findAll();
-        model.addAttribute("absences", absences);
-
-        return model;
+    @GetMapping("/all")
+    public @ResponseBody Iterable<Absence> getAllUsers() {
+        return repository.findAll();
     }
 
 
-
-
-    @RequestMapping(value = "/new", method = RequestMethod.GET)
-    public String newAbsence(Model model) {
-        model.addAttribute("absence", new Absence());
-        return "edit";
+    @GetMapping("/")
+    public String index(Model model)
+    {
+        model.addAttribute("absences", repository.findAll());
+        return "index";
     }
-
-    @RequestMapping(value="/edit/{id}", method=RequestMethod.POST)
-    public String editAbsence(@PathVariable Integer id, Model model) {
-        Absence absence = service.getAbsenceById(id);
-        model.addAttribute("absence", absence);
-        return "operations/edit";
-    }
-
-
 
 }
